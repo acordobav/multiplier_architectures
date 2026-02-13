9cfcdf0dd075685e1be85a60626debd768e21b56
Arturo Cordoba <arturocv16@gmail.com>
2026-02-12 21:16:42 -0600

    Change product width to 64 bits

--- booth/booth_multiplier_wrapper.v
+++ booth/booth_multiplier_wrapper.v
@@ -1,12 +1,12 @@
  
 module booth_multiplier_wrapper (
-    input  wire         clk,
-    input  wire         rst,
-    input  wire         start,
-    input  wire [63:0]  multiplicand,
-    input  wire [63:0]  multiplier,
-    output wire [127:0] product,
-    output wire         done
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        start,
+    input  wire [63:0] multiplicand,
+    input  wire [63:0] multiplier,
+    output wire [63:0] product,
+    output wire        done
 );
 
 booth_multiplier #(.N(64)) u0 (
